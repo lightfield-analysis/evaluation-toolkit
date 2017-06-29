af139532c4f60b30d1fa939eac7c0884dcfb142f
lightfield-benchmark <contact@lightfield-analysis.net>
2017-06-29 21:01:00 +0200

    added pairwise comparison figure

---
+++ source/plot_pairwise_comparisons.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+############################################################################
+#  This file is part of the 4D Light Field Benchmark.                      #
+#                                                                          #
+#  This work is licensed under the Creative Commons                        #
+#  Attribution-NonCommercial-ShareAlike 4.0 International License.         #
+#  To view a copy of this license,                                         #
+#  visit http://creativecommons.org/licenses/by-nc-sa/4.0/.                #
+#                                                                          #
+#  Authors: Katrin Honauer & Ole Johannsen                                 #
+#  Contact: contact@lightfield-analysis.net                                #
+#  Website: www.lightfield-analysis.net                                    #
+#                                                                          #
+#  The 4D Light Field Benchmark was jointly created by the University of   #
+#  Konstanz and the HCI at Heidelberg University. If you use any part of   #
+#  the benchmark, please cite our paper "A dataset and evaluation          #
+#  methodology for depth estimation on 4D light fields". Thanks!           #
+#                                                                          #
+#  @inproceedings{honauer2016benchmark,                                    #
+#    title={A dataset and evaluation methodology for depth estimation on   #
+#           4D light fields},                                              #
+#    author={Honauer, Katrin and Johannsen, Ole and Kondermann, Daniel     #
+#            and Goldluecke, Bastian},                                     #
+#    booktitle={Asian Conference on Computer Vision},                      #
+#    year={2016},                                                          #
+#    organization={Springer}                                               #
+#    }                                                                     #
+#                                                                          #
+############################################################################
+
+
+from utils.option_parser import *
+
+if __name__ == "__main__":
+    parser = OptionParser([AlgorithmOps(), SceneOps()])
+    algorithms, scenes = parser.parse_args()
+
+    # delay imports to speed up usage response
+    from evaluations import pairwise_comparisons
+    pairwise_comparisons.plot_pairwise_comparisons(algorithms, scenes)
